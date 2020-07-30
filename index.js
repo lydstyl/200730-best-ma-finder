23b27e051d31a41efcff6ba2c20d619279b1f1f6
Gabriel Brun <lydstyl@gmail.com>
2020-07-30 23:28:56 +0200

    % gain seems to works

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const csv = require('csv-parser')
 const fs = require('fs')
 
-let data = null
+const nbOfLine = 970
+let biggestMa = 50
+
+const settings = {
+  nbOfLine,
+  maLen: biggestMa,
+  numberOfLinesToUse: nbOfLine + biggestMa, // x days + ma200 is the biggest ma
+  comission: 0.0025,
+}
+
+global.data = null
 
 const getCsvData = async () => {
-  data = await (() => {
+  const data = await (() => {
     return new Promise((resolve, rejet) => {
       const results = []
 
@@ -20,25 +30,55 @@ const getCsvData = async () => {
       }
     })
   })()
+
+  return data.map((d) => {
+    return d
+  })
 }
 
 const findBestMa = async () => {
-  await getCsvData()
+  let data = await getCsvData()
 
-  removeTitlesAndChangeDataKeys()
+  global.data = removeTitlesAndChangeDataKeys(data)
 
-  addJsDateToData()
+  //addJsDateToData()
 
-  console.log('findBestMa -> data', data[0])
+  sliceData(settings.numberOfLinesToUse)
+
+  addMAverages()
+
+  data = addAmounts()
+  console.log(
+    'findBestMa -> data',
+    Array.isArray(data),
+    data.length,
+    data[data.length - 1]
+  )
+
+  // console.log('findBestMa -> global.data', global.data[0])
 }
 
-const removeTitlesAndChangeDataKeys = () => {
+const removeTitlesAndChangeDataKeys = (data) => {
   const titles = data.shift()
 
   return data.map((d) => {
     const newData = {}
     for (let i = 0; i <= 7; i++) {
+      if (
+        titles[i] === 'Open' ||
+        titles[i] === 'High' ||
+        titles[i] === 'Low' ||
+        titles[i] === 'Volume BTC' ||
+        titles[i] === 'Volume USD'
+      ) {
+        continue
+      }
+
       newData[titles[i]] = d[i]
+
+      if (titles[i] === 'Close') {
+        newData[titles[i]] = parseFloat(d[i])
+      }
     }
 
     return newData
@@ -46,7 +86,104 @@ const removeTitlesAndChangeDataKeys = () => {
 }
 
 const addJsDateToData = () => {
-  return data.map((d) => ({ ...d, jsDate: new Date(d.Date) }))
+  data = data.map((d) => ({ ...d, JsDate: new Date(d.Date) }))
+}
+
+const sliceData = (lineNb) => {
+  data = data.slice(0, lineNb)
+}
+
+const addMAverages = () => {
+  newLines = []
+
+  for (let i = 0; i <= nbOfLine; i++) {
+    const line = data[i]
+
+    let ma2Data = data.slice(i, i + settings.maLen)
+
+    ma2Data = ma2Data.map((m) => {
+      return m.Close
+    })
+
+    line.ma2 =
+      ma2Data.reduce((accumulator, currentValue) => {
+        return parseFloat(accumulator) + parseFloat(currentValue)
+      }) / settings.maLen
+
+    if (line.Close > line.ma2) {
+      line.isCloseAboveMa2 = true
+    }
+
+    newLines.push(line)
+  }
+
+  return newLines
+}
+
+const addAmounts = () => {
+  // first  we don't have BTC
+
+  let lines = data.slice(0, settings.nbOfLine).reverse()
+
+  lines[0].totalUSD = 100
+  lines[0].cryptoNb = 0
+  lines[0].usdValue = 100
+  lines[0].operation = 'sell'
+  console.log('addAmounts -> lines[0]', lines[0])
+
+  let previousLine = lines[0]
+
+  lines.map((l, index) => {
+    if (index) {
+      previousLine = lines[index - 1]
+
+      if (!l.totalUSD) {
+        l.totalUSD = previousLine.totalUSD
+      }
+
+      if (!l.cryptoNb) {
+        l.cryptoNb = previousLine.cryptoNb
+      }
+    }
+
+    if (l.isCloseAboveMa2) {
+      l.operation = 'buy'
+    } else {
+      l.operation = 'sell'
+    }
+
+    l.isPreviousOperationDifferente = l.operation !== previousLine.operation
+
+    if (previousLine.Date === l.Date) {
+      l.isPreviousOperationDifferente = true
+    }
+
+    if (l.isPreviousOperationDifferente) {
+      if (l.operation === 'buy') {
+        l.usdValue = l.totalUSD * (1 - settings.comission)
+
+        l.cryptoNb = (l.totalUSD / l.Close) * (1 - settings.comission)
+
+        l.totalUSD = 0
+      } else if (l.operation === 'sell') {
+        l.operation = 'sell'
+
+        l.usdValue = l.cryptoNb * l.Close * (1 - settings.comission)
+
+        l.cryptoNb = 0
+
+        l.totalUSD = l.usdValue
+      }
+    } else {
+      l.usdValue = previousLine.usdValue
+    }
+
+    return l
+  })
+
+  data = lines
+
+  return lines
 }
 
 findBestMa()
